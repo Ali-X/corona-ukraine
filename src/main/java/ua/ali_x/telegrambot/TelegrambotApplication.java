60a0841193bbf0d0590815681730d94e4976b275
a.neshchebnaya <alina.neshchebnaya@sagax.software>
2020-04-30 18:21:54 +0300

    Replace tomcat with jetty

--- src/main/java/ua/ali_x/telegrambot/TelegrambotApplication.java
+++ src/main/java/ua/ali_x/telegrambot/TelegrambotApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.telegram.telegrambots.ApiContextInitializer;
